ff9d5f8f280d9442d812a153ab83e3d6d7764f1a
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-02-20 16:22:08 -0500

    Create Task model

---
+++ TodoApi/Models/Task.cs
@@ -0,0 +1,11 @@
+namespace TodoApi.Models;
+using Microsoft.OData.Edm;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsComplete { get; set; }
+    public Date CreationTime { get; set; }
+}
