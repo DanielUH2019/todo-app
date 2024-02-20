36e6de6d80055106991de467e9d0f3eb79240080
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-02-20 16:23:14 -0500

    Add Odata and entity framework config

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,11 +1,62 @@
+// Program.cs
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+// OData namespaces
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Create the service EDM model.
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntitySet<Task>("Tasks");
+var edmModel = modelBuilder.GetEdmModel();
+
+// Register OData service.
+builder.Services.AddControllers().AddOData(
+    options => options.EnableQueryFeatures(null).AddRouteComponents(
+        routePrefix: "odata",
+        model: modelBuilder.GetEdmModel()));
+
+
+builder.Services.AddDbContext<TaskDbContext>(options =>
+    options.UseInMemoryDatabase("TaskDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvcCore(options =>
+{
+    foreach (var outputFormatter in options.OutputFormatters.OfType<OutputFormatter>().Where(x => x.SupportedMediaTypes.Count == 0))
+    {
+        outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+    }
+
+    foreach (var inputFormatter in
+
+    options.InputFormatters.OfType<InputFormatter>().Where(
+
+    x => x.SupportedMediaTypes.Count == 0))
+
+    {
+
+        inputFormatter.SupportedMediaTypes.Add(
+
+        new MediaTypeHeaderValue(
+
+        "application/prs.odatatestxx-odata"));
+
+    }
+
+});
 
 var app = builder.Build();
 
@@ -18,8 +69,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
