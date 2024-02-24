d93a73d68b61aca948daf8cebfbc4e5f63ab853f
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-02-24 16:46:22 -0500

    remove unnecesary fields in TaskModel

--- todo-ui/src/models/task.d.ts
+++ todo-ui/src/models/task.d.ts
@@ -3,9 +3,8 @@ import { z } from "zod";
 export const taskModel = z.object({
   id: z.number(),
   name: z.string(),
-  description: z.string(),
   isCompleted: z.boolean(),
-  createdAt: z.string(),
+  createdAt: z.date(),
 });
 
 export const taskModelList = z.array(taskModel);
