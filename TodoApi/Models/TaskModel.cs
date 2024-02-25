ea35e0488d860b81d2b13dbc194982b064d4ddc5
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-02-25 12:33:18 -0500

    Remove description from TaskModel

--- TodoApi/Models/TaskModel.cs
+++ TodoApi/Models/TaskModel.cs
@@ -5,7 +5,6 @@ public class TaskModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
     public bool IsComplete { get; set; }
     public DateTimeOffset CreationTime { get; set; }
 }
